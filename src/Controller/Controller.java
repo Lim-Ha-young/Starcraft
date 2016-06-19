a0d1596b645fc250f9ef776a7d67e9926b6d8d70
Lim-Ha-young <sens921119@naver.com>
2016-06-19 17:52:34 +0900

    changed

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -10,45 +10,40 @@ public class Controller {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		Marine m1 = new Marine(10,"m1");
-		Marine m2 = new Marine(20,"m2");
-		FireBet f = new FireBet("f1",200,10);
-		
-		
-		m1.seeState();
-		m2.seeState();
-		
-		int i = 0;
-		while(i==0){
-			System.out.println("-------------------------------");
-			System.out.println("1.공격 2. UPGRADE 3.상태보기 (종료하려면 아무키나)");
-			System.out.println("-------------------------------");
-			int num = scanner.nextInt();
-			switch(num){
-			case 1 :
-				m1.Attack(m2);
-				break;
-			case 2 :
-				m1.Upgrade();
-				break;
-			case 3 :
-				m1.seeState();
-				break;
+		Marine m1 = new Marine(200,10,"m1");
+		Marine m2 = new Marine(100,20,"m2");
+		FireBet f1 = new FireBet(100,10,"f1");
 
-			default :
-				i=1;
-				break;
-			}
-		}
-		
-		m1.getHp();
-		
-		
-		m2.Attack(m1);
-		m1.Attack(f);
 	
-		m1.Attack(f);
-		m1.Attack(f);
+		m1.Attack(f1);
+		f1.seeState();
+		m1.Attack(f1);
+		f1.seeState();
+		
+		
+//		int i = 0;
+//		while(i==0){
+//			System.out.println("-------------------------------");
+//			System.out.println("1.공격 2. UPGRADE 3.상태보기 (종료하려면 아무키나)");
+//			System.out.println("-------------------------------");
+//			int num = scanner.nextInt();
+//			switch(num){
+//			case 1 :
+//				m1.Attack(m2);
+//				break;
+//			case 2 :
+//				m1.Upgrade();
+//				break;
+//			case 3 :
+//				m1.seeState();
+//				break;
+//
+//			default :
+//				i=1;
+//				break;
+//			}
+//		}
+//		
 	}
 
 }
--- src/Model/Unit.java
+++ src/Model/Unit.java
@@ -5,15 +5,38 @@ public class Unit {
 	protected int hp;
 	protected int hit;
 	protected String name;
+	public boolean isDead=false;
 	
-	public Unit(int hp,int hit, String name){
+	public Unit(int hp, int hit, String name){
 		this.hp=hp;
 		this.hit=hit;
 		this.name = name;
 	}
 	public void seeState(){
-		System.out.println(hp);
-		System.out.println(hit);
-		System.out.println(name);
+		System.out.println(name+"의 HP:"+hp);
+		System.out.println("     HIT:"+hit+"\n");
+
+	}
+	public void Attack(Unit unit){
+		if(unit.hp<=0){
+			this.isDead=true;
+			System.out.println("죽음");
+		}
+		else{
+		System.out.println("unit ATTACK!");
+		unit.hp=unit.hp-this.hit;
+		
+		
+		System.out.println(this.name+"이 "+unit.name +"을 공격한당\n");
+		}
+		if(unit.hp<=0){
+			System.out.println("더이상 공격 ㄴㄴ");
+		}
+		
+	}
+	
+	public void Upgrade(){
+		this.hit=this.hit+10;
+		System.out.println(this.name+"의 공격력이 "+this.hit+" 되었습니다.\n");
 	}
 }
